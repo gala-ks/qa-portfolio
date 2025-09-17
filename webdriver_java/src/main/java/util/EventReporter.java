a808415bbfe8d56d79d65f2d965346b813308dc2
Gala107 <wgalina@hotmail.com>
2025-09-17 17:50:31 -0500

    Created an implementation for WebDriverListener and created a decorated driver to catch those events

---
+++ webdriver_java/src/main/java/util/EventReporter.java
@@ -0,0 +1,13 @@
+package util;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.events.WebDriverListener;
+
+public class EventReporter implements WebDriverListener{
+
+    @Override
+    public void beforeClick(WebElement element) {
+        System.out.println("Clicking on: " + element.getText());
+    }
+
+}
--- webdriver_java/src/test/java/base/BaseTests.java
+++ webdriver_java/src/test/java/base/BaseTests.java
@@ -5,12 +5,14 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringDecorator;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import util.EventReporter;
 import util.WindowNavigation;
 
 import java.io.File;
@@ -20,6 +22,7 @@ import java.time.Duration;
 public abstract class BaseTests
 {
     private WebDriver driver;
+    private WebDriver decorated;
     protected HomePage homePage;
 
     @BeforeClass
@@ -28,11 +31,19 @@ public abstract class BaseTests
         driver = new ChromeDriver();
 
         /*
+            Replace the driver with a decorated driver that uses an event listener to log actions.
+            This is a new way to do it in Selenium 4.0+
+            See util/EventReporter.java for the event listener class.
+            I'm not using it in this project, but it's here to show how it works.
+         */
+        decorated = new EventFiringDecorator(new EventReporter()).decorate(driver);
+
+        /*
           Set a default wait time for finding elements.
           It's better to use explicit waits, but this is included to show how it works.
           Implicit waits are set for the life of the WebDriver instance.
         */
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
         goHome();
         homePage = new HomePage(driver);
@@ -66,5 +77,9 @@ public abstract class BaseTests
 
     public WindowNavigation getWindowNavigator() {
         return new WindowNavigation(driver);
+    }
+
+    public WebDriver getDecoratedDriver() {
+        return decorated;
     }
 }
